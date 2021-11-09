4aeb9a7f135d04234ea6a2b6409e966c348a7821
NguyenJohnnyT <johnnytrucnguyen@gmail.com>
2021-11-08 16:04:32 -0800

    add hello message

--- app/bot.py
+++ app/bot.py
@@ -3,12 +3,30 @@ import logging
 from config import read_from_env
 from discord_slash import SlashCommand, SlashContext
 
+# Read cofig from environment variables
 bot_token, guild_id, _ = read_from_env()
 
+# Enable basic logging into console
 logging.basicConfig(level=logging.INFO)
 
+# Set up a new bot instance
 bot = commands.Bot(command_prefix="")
 
+# Bot handles slash commands
 slash = SlashCommand(bot, sync_commands=True)
 
+@bot.event
+async def on_ready():
+  """Show log message when Bot has connected to Discord"""
+  logging.info("Bot is now online in Discord")
+
+@bot.event
+async def on_message(message):
+  """Reply to any hello bot message in the channel"""
+  if message.author == bot.user:
+    return
+  if message.content == "!hello" or "!Hello":
+    await message.channel.send("I am MCY bot")
+
+# Run the bot
 bot.run(bot_token)
